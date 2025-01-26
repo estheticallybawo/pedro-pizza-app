77e461cef6ef8d1977cdc03d79bb2ba5b6203c29
Esther <estheticallybawo@gmail.com>
2025-01-26 19:36:05 +0100

    Proxy

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,25 @@ import Pizza from "./Pizza";
 const App = () => {
   return (
     <div>
-      <h1>Padre Gino's Pizza – Order Now</h1>
-      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
-      <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese"
+      <h1 className="logo">Padre Gino's Pizza</h1>
+      <Pizza
+        name="Pepperoni"
+        description="Mozzarella Cheese, Pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
       />
-      <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+      <Pizza
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image={"/public/pizzas/big_meat.webp"}
       />
     </div>
   );
 };
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
